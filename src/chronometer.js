7286707502ad6e50d625f068d7fa7ef4f2e2015a
Juan Yuste Del Valle <kurobaa@gmail.com>
2024-01-18 15:58:45 +0100

    first commit

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,18 +1,32 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    (this.currentTime = 0), (this.intervalId = null);
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    setInterval(() => {
+      //printTimeCallback(this.currentTime);
+      this.currentTime += 1;
+      if (printTimeCallback) printTimeCallback();
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
     // ... your code goes here
+    let seconds = this.currentTime;
+    if (seconds === 0) {
+      return seconds;
+    } else {
+      let remainingSeconds = seconds % 60;
+      return remainingSeconds;
+    }
   }
 
   computeTwoDigitNumber(value) {
